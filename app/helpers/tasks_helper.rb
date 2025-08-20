be163214b774d12c19756bd4cb06d4518f9651cb
Sattrawoot Parnemeng <sattrawootparnemeng@MacBook-Air-khxng-Sattrawoot.local>
2025-08-20 11:25:48 +0700

    create method heler task and change style in tak page

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,5 @@
 module TasksHelper
+     def task_complete_class(task)
+    "line-through text-gray-400" if task.completed
+     end
 end
--- spec/helpers/tasks_helper_spec.rb
+++ spec/helpers/tasks_helper_spec.rb
@@ -11,5 +11,16 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe TasksHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+    describe "#task_complete_class" do
+    let(:completed_task) { double("Task", completed: true) }
+    let(:incomplete_task) { double("Task", completed: false) }
+
+    it "returns the correct class for completed task" do
+      expect(helper.task_complete_class(completed_task)).to eq("line-through text-gray-400")
+    end
+
+    it "returns nil for incomplete task" do
+      expect(helper.task_complete_class(incomplete_task)).to be_nil
+    end
+  end
 end
